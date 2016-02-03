e5cc2664e189f21a3ae7f05b36b1771b48033bdc
COREY FOGG <cfogg6@gatech.edu>
2016-02-02 21:50:03 -0500

    Added Welcome Activity

---
+++ src/main/java/com/mymovieapp/WelcomeActivity.java
@@ -0,0 +1,63 @@
+package com.mymovieapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Corey on 2/2/16.
+ */
+public class WelcomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button registerButton = (Button) findViewById(R.id.btn_registration);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(WelcomeActivity.this, RegistrationActivity.class);
+                startActivity(it);
+            }
+        });
+        Button loginButton = (Button) findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(it);
+            }
+        });
+        TextView title = (TextView) findViewById(R.id.tv_login_title);
+        title.setText("Popcorn Salt");
+        EditText username = (EditText) findViewById(R.id.et_username);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

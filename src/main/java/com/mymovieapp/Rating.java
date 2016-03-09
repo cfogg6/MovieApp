c234b83edef852f46ea078c6c1d1fd2ee50df108
Honey Chawla <honeychawla96@gmail.com>
2016-03-09 00:06:25 -0500

    Recommendations works, baby ;D

--- src/main/java/com/mymovieapp/Rating.java
+++ src/main/java/com/mymovieapp/Rating.java
@@ -1,5 +1,10 @@
 package com.mymovieapp;
 
+import android.util.Log;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
 /**
  * Created by Corey on 3/8/16.
  */
@@ -7,16 +12,41 @@ public class Rating implements Comparable {
 
     String name;
     String username;
-    double rating;
+    ArrayList<Double> list = new ArrayList<>();
 
-    public Rating(String name, String username, double rating) {
+    public Rating(String name, String username) {
         this.name = name;
         this.username = username;
-        this.rating = rating;
     }
 
     @Override
     public int compareTo(Object rating) {
-        return 0;
+        if (this.getAverageRating() < ((Rating) rating).getAverageRating()) {
+            return 1;
+        } else if (this.getAverageRating() > ((Rating) rating).getAverageRating()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Rating)) {
+            return false;
+        }
+        return name.equals(((Rating)other).name);
+    }
+
+    public void addRating(Double rating) {
+        list.add(rating);
+    }
+
+    public double getAverageRating() {
+        DecimalFormat format = new DecimalFormat("#.#");
+        double sum = 0;
+        for (Double item: list)
+            sum += item;
+        return Double.valueOf(format.format(sum / list.size()));
     }
 }
--- src/main/java/com/mymovieapp/RecommendationsActivity.java
+++ src/main/java/com/mymovieapp/RecommendationsActivity.java
@@ -2,17 +2,21 @@ package com.mymovieapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.ParseUser;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -21,12 +25,16 @@ import java.util.List;
 public class RecommendationsActivity extends Activity {
     JSONObject movieReviewJSON = new JSONObject();
     Spinner spinner;
+    ArrayList<Rating> ratings = new ArrayList<>();
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendations);
         spinner = (Spinner) findViewById(R.id.majors_spinner);
+        listView = (ListView) findViewById(R.id.lv_ratings);
+        listView.setAdapter(new RatingsListAdapter(this, 0));
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.majors_array, android.R.layout.simple_spinner_item);
@@ -35,20 +43,51 @@ public class RecommendationsActivity extends Activity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+        spinner.setOnItemSelectedListener((new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                updateList();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        }));
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        updateList();
+    }
+
+    private void updateList() {
+        ratings = new ArrayList<>();
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Ratings");
         query.whereEqualTo("major", spinner.getSelectedItem().toString());
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> list, ParseException e) {
-                for(ParseObject element: list) {
-                    double rating = element.getDouble("rating");
+                if (e == null) {
+                    for (ParseObject element : list) {
+                        Rating newRating = new Rating(element.getString("title"),
+                                element.getString("username"));
+                        newRating.addRating(element.getDouble("rating"));
 
+                        int index = ratings.lastIndexOf(newRating);
+                        if (index > -1) {
+                            ratings.get(index).addRating(element.getDouble("rating"));
+                        } else {
+                            ratings.add(newRating);
+                        }
+                    }
+                    Collections.sort(ratings);
+                } else {
+                    e.printStackTrace();
                 }
+                ((RatingsListAdapter) listView.getAdapter()).updateRatings(ratings);
+                ((RatingsListAdapter) listView.getAdapter()).notifyDataSetChanged();
             }
         });
     }

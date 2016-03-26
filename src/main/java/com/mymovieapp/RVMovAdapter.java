fcf0bc8c6415ac8751769dc228d0d326ea1412e9
Honey Chawla <honeychawla96@gmail.com>
2016-03-25 22:16:42 -0400

    Merging ratings functionality
    # Conflicts:
    #	.gitmodules
    
    Merging dev with master for movie recommendations
    
    Populating new DVDs and releases!!!! :D

--- src/main/java/com/mymovieapp/RVMovAdapter.java
+++ src/main/java/com/mymovieapp/RVMovAdapter.java
@@ -1,8 +1,12 @@
 package com.mymovieapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +14,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 import java.util.List;
 
 /**
  * Created by Angelo on 3/19/2016.
  */
 public class RVMovAdapter extends RecyclerView.Adapter<RVMovAdapter.MovieViewHolder> {
-
     public static class MovieViewHolder extends RecyclerView.ViewHolder {
         CardView cv;
         TextView movieName;
@@ -62,14 +70,43 @@ public class RVMovAdapter extends RecyclerView.Adapter<RVMovAdapter.MovieViewHol
 
     @Override
     public void onBindViewHolder(MovieViewHolder movieViewHolder, int i) {
-        movieViewHolder.movieName.setText("PlaceHolder name");
-        movieViewHolder.releaseDate.setText("1990");
+        NewMovieDrawerActivity.Movie mov = movies.get(i);
+
+        movieViewHolder.movieName.setText(mov.name);
+        movieViewHolder.releaseDate.setText(mov.date);
         movieViewHolder.details.setText("all details");
-        movieViewHolder.movPhoto.setImageResource(R.drawable.profile_person);
+        //movieViewHolder.movPhoto.setImageResource(R.drawable.ic_launcher);
+        new DownloadImageTask(movieViewHolder.movPhoto).execute(mov.photoId);
     }
 
     @Override
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+
     }
 }

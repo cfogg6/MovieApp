497db749f5673bf9a493d9c03b4f01cbab0308d6
COREY FOGG <cfogg6@gatech.edu>
2016-04-05 23:07:53 -0400

    fixed admin lists

--- src/main/java/com/mymovieapp/MovieInfoActivity.java
+++ src/main/java/com/mymovieapp/MovieInfoActivity.java
@@ -4,15 +4,22 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -61,6 +68,7 @@ public class MovieInfoActivity extends BackToolbarActivity {
 
         movPic = (AppCompatImageView) findViewById(R.id.iV_movPhoto);
         synopsis = (TextView) findViewById(R.id.tV_synopsis);
+        makeTextViewResizable(synopsis, 3, "View More", true);
         rv = (RecyclerView) findViewById(R.id.comments_rv);
         movieObject = (getIntent().getParcelableExtra("SALTY_POPCORN_CURRENT_MOVIE"));
         starBar.setRating(0);
@@ -233,4 +241,98 @@ public class MovieInfoActivity extends BackToolbarActivity {
         }
 
     }
+
+    public static void makeTextViewResizable(final TextView tv,
+                                             final int maxLine, final String expandText, final boolean viewMore) {
+
+        if (tv.getTag() == null) {
+            tv.setTag(tv.getText());
+        }
+        ViewTreeObserver vto = tv.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onGlobalLayout() {
+
+                ViewTreeObserver obs = tv.getViewTreeObserver();
+                obs.removeGlobalOnLayoutListener(this);
+                if (maxLine == 0) {
+                    int lineEndIndex = tv.getLayout().getLineEnd(0);
+                    String text = tv.getText().subSequence(0,
+                            lineEndIndex - expandText.length() + 1)
+                            + " " + expandText;
+                    tv.setText(text);
+                    tv.setMovementMethod(LinkMovementMethod.getInstance());
+                    tv.setText(
+                            addClickablePartTextViewResizable(tv.getText()
+                                            .toString(), tv, maxLine, expandText,
+                                    viewMore), TextView.BufferType.SPANNABLE);
+                } else if (maxLine > 0 && tv.getLineCount() >= maxLine) {
+                    int lineEndIndex = tv.getLayout().getLineEnd(maxLine - 1);
+                    String text = tv.getText().subSequence(0,
+                            lineEndIndex - expandText.length() + 1)
+                            + " " + expandText;
+                    tv.setText(text);
+                    tv.setMovementMethod(LinkMovementMethod.getInstance());
+                    tv.setText(
+                            addClickablePartTextViewResizable(tv.getText()
+                                            .toString(), tv, maxLine, expandText,
+                                    viewMore), TextView.BufferType.SPANNABLE);
+                } else {
+                    int lineEndIndex = tv.getLayout().getLineEnd(
+                            tv.getLayout().getLineCount() - 1);
+                    String text = tv.getText().subSequence(0, lineEndIndex)
+                            + " " + expandText;
+                    tv.setText(text);
+                    tv.setMovementMethod(LinkMovementMethod.getInstance());
+                    tv.setText(
+                            addClickablePartTextViewResizable(tv.getText()
+                                            .toString(), tv, lineEndIndex, expandText,
+                                    viewMore), TextView.BufferType.SPANNABLE);
+                }
+            }
+        });
+
+    }
+
+    private static SpannableStringBuilder addClickablePartTextViewResizable(
+            final String strSpanned, final TextView tv, final int maxLine,
+            final String spanableText, final boolean viewMore) {
+        SpannableStringBuilder ssb = new SpannableStringBuilder(strSpanned);
+
+        if (strSpanned.contains(spanableText)) {
+            ssb.setSpan(
+                    new ClickableSpan() {
+
+                        @Override
+                        public void onClick(View widget) {
+
+                            if (viewMore) {
+                                tv.setLayoutParams(tv.getLayoutParams());
+                                tv.setText(tv.getTag().toString(),
+                                        TextView.BufferType.SPANNABLE);
+                                tv.invalidate();
+                                makeTextViewResizable(tv, -5, "...Read Less",
+                                        false);
+                                tv.setTextColor(Color.BLACK);
+                            } else {
+                                tv.setLayoutParams(tv.getLayoutParams());
+                                tv.setText(tv.getTag().toString(),
+                                        TextView.BufferType.SPANNABLE);
+                                tv.invalidate();
+                                makeTextViewResizable(tv, 5, "...Read More",
+                                        true);
+                                tv.setTextColor(Color.BLACK);
+                            }
+
+                        }
+                    }, strSpanned.indexOf(spanableText),
+                    strSpanned.indexOf(spanableText) + spanableText.length(), 0);
+
+        }
+        return ssb;
+
+    }
+
 }
--- src/main/java/com/mymovieapp/MyClickableSpan.java
+++ src/main/java/com/mymovieapp/MyClickableSpan.java
@@ -3,108 +3,30 @@ package com.mymovieapp;
 /**
  * Created by Corey on 4/4/16.
  */
-import android.text.Html;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
 import android.text.TextPaint;
-import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
-import android.view.ViewTreeObserver;
-import android.widget.TextView;
 
 public class MyClickableSpan extends ClickableSpan {
 
-    private boolean isUnderline = true;
+        private boolean isUnderline = true;
 
-    /**
-     * Constructor
-     */
-    public MyClickableSpan(boolean isUnderline) {
-        this.isUnderline = isUnderline;
-    }
+        /**
+         * Constructor
+         */
+        public MyClickableSpan(boolean isUnderline) {
+            this.isUnderline = isUnderline;
+        }
 
-    @Override
-    public void updateDrawState(TextPaint ds) {
+        @Override
+        public void updateDrawState(TextPaint ds) {
 
-        ds.setUnderlineText(isUnderline);
-
-    }
-
-    @Override
-    public void onClick(View widget) {
-
-    }
-
-    private static SpannableStringBuilder addClickablePartTextViewResizable(final Spanned strSpanned, final TextView tv,
-                                                                            final int maxLine, final String spanableText, final boolean viewMore) {
-        String str = strSpanned.toString();
-        SpannableStringBuilder ssb = new SpannableStringBuilder(strSpanned);
-
-        if (str.contains(spanableText)) {
-
-
-            ssb.setSpan(new MyClickableSpan(false) {
-                @Override
-                public void onClick(View widget) {
-                    if (viewMore) {
-                        tv.setLayoutParams(tv.getLayoutParams());
-                        tv.setText(tv.getTag().toString(), TextView.BufferType.SPANNABLE);
-                        tv.invalidate();
-                        makeTextViewResizable(tv, -1, "View Less", false);
-                    } else {
-                        tv.setLayoutParams(tv.getLayoutParams());
-                        tv.setText(tv.getTag().toString(), TextView.BufferType.SPANNABLE);
-                        tv.invalidate();
-                        makeTextViewResizable(tv, 3, "View More", true);
-                    }
-                }
-            }, str.indexOf(spanableText), str.indexOf(spanableText) + spanableText.length(), 0);
+            ds.setUnderlineText(isUnderline);
 
         }
-        return ssb;
+
+        @Override
+        public void onClick(View widget) {
+
+        }
     }
-
-        public static void makeTextViewResizable(final TextView tv, final int maxLine, final String expandText, final boolean viewMore) {
-
-            if (tv.getTag() == null) {
-                tv.setTag(tv.getText());
-            }
-            ViewTreeObserver vto = tv.getViewTreeObserver();
-            vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-
-                @SuppressWarnings("deprecation")
-                @Override
-                public void onGlobalLayout() {
-
-                    ViewTreeObserver obs = tv.getViewTreeObserver();
-                    obs.removeGlobalOnLayoutListener(this);
-                    if (maxLine == 0) {
-                        int lineEndIndex = tv.getLayout().getLineEnd(0);
-                        String text = tv.getText().subSequence(0, lineEndIndex - expandText.length() + 1) + " " + expandText;
-                        tv.setText(text);
-                        tv.setMovementMethod(LinkMovementMethod.getInstance());
-                        tv.setText(
-                                addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, maxLine, expandText,
-                                        viewMore), TextView.BufferType.SPANNABLE);
-                    } else if (maxLine > 0 && tv.getLineCount() >= maxLine) {
-                        int lineEndIndex = tv.getLayout().getLineEnd(maxLine - 1);
-                        String text = tv.getText().subSequence(0, lineEndIndex - expandText.length() + 1) + " " + expandText;
-                        tv.setText(text);
-                        tv.setMovementMethod(LinkMovementMethod.getInstance());
-                        tv.setText(
-                                addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, maxLine, expandText,
-                                        viewMore), TextView.BufferType.SPANNABLE);
-                    } else {
-                        int lineEndIndex = tv.getLayout().getLineEnd(tv.getLayout().getLineCount() - 1);
-                        String text = tv.getText().subSequence(0, lineEndIndex) + " " + expandText;
-                        tv.setText(text);
-                        tv.setMovementMethod(LinkMovementMethod.getInstance());
-                        tv.setText(
-                                addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, lineEndIndex, expandText,
-                                        viewMore), TextView.BufferType.SPANNABLE);
-                    }
-                }
-            });
-    }
-}
